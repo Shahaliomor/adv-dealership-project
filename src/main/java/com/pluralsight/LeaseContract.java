3aa7c6357e7581f681f4c7c4f062178269c90388
Shah Ali Omor <omoranam@gmail.com>
2026-05-19 19:30:27 -0400

    complete lease contract class

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
 
-public class LeaseContract {
+public class LeaseContract extends Contract {
+
+    final double LEASE_FEE_RATE = 0.07;
+    final double ENDING_VALUE_RATE = 0.50;
+
+    public LeaseContract(String date,
+                         String customerName,
+                         String customerEmail,
+                         Vehicle vehicleSold) {
+
+        super(date, customerName, customerEmail, vehicleSold);
+    }
+
+    @Override
+    public double getTotalPrice() {
+
+        double price = getVehicleSold().getPrice();
+
+        double endingValue = price * ENDING_VALUE_RATE;
+
+        double leaseFee = price * LEASE_FEE_RATE;
+
+        return endingValue + leaseFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+
+        double totalPrice = getTotalPrice();
+
+        double monthlyRate = .04 / 12;
+
+        int months = 36;
+
+        return totalPrice *
+                ((monthlyRate * Math.pow(1 + monthlyRate, months))
+                        /(Math.pow(1 + monthlyRate, months) - 1));
+    }
 }
